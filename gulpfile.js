194ced846d4677c065f27300adb3c733d0f1802c
BuildTools <unconfigured@null.spigotmc.org>
2018-09-30 23:34:01 +0300

    updated gulpfile.js tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,58 @@
-var gulp = require('gulp');
-var bs = require('browser-sync');
-var concatCSS = require('gulp-concat-css');
-var less = require('gulp-less');
+var gulp       = require('gulp'),
+    less         = require('gulp-less'),
+    browserSync  = require('browser-sync'),
+    concatCSS    = require('gulp-concat-css'),
+    del          = require('del'),
+    autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('default', ['server'], function () {
-    gulp.watch('assets/less/*.less', [less]);
+gulp.task('less', function(){
+    return gulp.src('src/assets/themes/base/styles/*.less')
+        .pipe(less())
+        .pipe(autoprefixer(['last 2 versions', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'],
+            { cascade: true })) // Создаем префиксы
+        .pipe(concatCSS('style.css'))
+        .pipe(gulp.dest('src/assets/themes/base/css'))
+        .pipe(browserSync.reload({stream: true}))
 });
 
-gulp.task('server', function () {
-    bs.init({
+gulp.task('browser-sync', function() {
+    browserSync({
         server: {
-            baseDir: './'
+            baseDir: 'src' // Директория для сервера
         },
         notify: false
     });
-    gulp.watch('./*.html').on('change', bs.reload);
 });
 
-gulp.task('less', function () {
-    gulp.src('./assets/less/*.less')
-        .pipe(less())
-        .pipe(concatCSS('style.css'))
-        .pipe(gulp.dest('./assets/css'))
-        .pipe(bs.stream());
+gulp.task('watch', ['browser-sync'], function() {
+    gulp.watch('src/assets/themes/base/styles/*.less', ['less']); // Наблюдение за less файлами
+    gulp.watch('src/*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
+    gulp.watch('src/assets/scripts/*.js', browserSync.reload);   // Наблюдение за JS файлами
 });
+
+gulp.task('clean', function() {
+    return del.sync('dist'); // Удаляем папку dist перед сборкой
+});
+
+gulp.task('img', function() {
+    return gulp.src('src/assets/themes/base/img/**/*')
+        .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('build', ['clean', 'img', 'less'], function() {
+
+    var buildCss = gulp.src(['src/assets/themes/base/css/style.css'])
+        .pipe(gulp.dest('dist/css'));
+
+    var buildFonts = gulp.src('src/assets/themes/base/fonts/*')
+        .pipe(gulp.dest('dist/fonts'));
+
+    var buildJs = gulp.src('src/assets/scripts/*')
+        .pipe(gulp.dest('dist/js'));
+
+    var buildHtml = gulp.src('src/*.html')
+        .pipe(gulp.dest('dist'));
+
+});
+
+gulp.task('default', ['watch']);
